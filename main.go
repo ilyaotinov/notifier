e0c2432bae8e475ba38b60e9dc2e73e72c1ecf79
Ilya Otinov <ilya.otinov@gmail.com>
2026-08-31 16:42:45 +0300

    doc: add basic todos

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// TODO: add migraions
+// TODO: run migraions
+// TODO: create http server
+// TODO: run http server with [POST]/notification endpoint
+// TODO: add [GET]/notification endpoint
+// TODO: when basic endpoints will work, add sending to telegram bot notifications
+
+func main() {
+	fmt.Println("Hello Notifier!")
+}
