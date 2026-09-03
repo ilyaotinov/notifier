0b5499ca7a1f27831170f8e39c278b8b76da169e
Ilya Otinov <ilya.otinov@gmail.com>
2026-09-03 17:29:47 +0300

    feat: implement basic auth middleware

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ilyaotinov/notifier
 
 go 1.26.3
+
+require github.com/mattn/go-sqlite3 v1.14.50 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
 
-// TODO: add migraions
-// TODO: run migraions
-// TODO: create http server
-// TODO: run http server with [POST]/notification endpoint
-// TODO: add [GET]/notification endpoint
-// TODO: when basic endpoints will work, add sending to telegram bot notifications
+	_ "github.com/mattn/go-sqlite3"
+)
+
+/*
+   MVP user paths:
+   - Client send notification create request with http api
+   - This request protected with basic auth, just for case someone find out my open URL
+   - Deamon once in 10 seconds reads not sended notificatons and send it to specific telegram chat ID with specific bot token, both defined as OS.env
+*/
+
+const appDir = ".notifier"
+const appDBFile = "notifier.db"
+
+type Config struct {
+	Username string
+	Password string
+}
+
+func initConfig() (Config, error) {
+	username := os.Getenv("NOTIFIER_USERNAME")
+	if username == "" {
+		return Config{}, errors.New("NOTIFIER_USERNAME env is not set")
+	}
+
+	password := os.Getenv("NOTIFIER_PASSWORD")
+	if password == "" {
+		return Config{}, errors.New("NOTIFIER_PASSWORD env is not set")
+	}
+
+	return Config{
+		Username: username,
+		Password: password,
+	}, nil
+}
 
 func main() {
-	fmt.Println("Hello Notifier!")
+	conf, err := initConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := openAppDB()
+	if err != nil {
+		panic(err)
+	}
+
+	h := newHandler(db, conf)
+	registerRoutes(h)
+
+	err = http.ListenAndServe(":8090", nil) // TODO: move parameter to config
+	if err != nil {
+		panic(err)
+	}
+}
+
+func registerRoutes(h *Handler) {
+	http.Handle("POST /notification/{$}", h.basicAuth(http.HandlerFunc(h.createNotificationHandler)))
+}
+
+func openAppDB() (*sql.DB, error) {
+	appDir, err := createAppDirIfNotExists()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create application directory: %w", err)
+	}
+
+	db, err := sql.Open("sqlite3", filepath.Join(appDir, appDBFile))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	// TODO: run migrations
+
+	return db, nil
+}
+
+func createAppDirIfNotExists() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	path := filepath.Join(homeDir, appDir)
+	err = os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
 }
